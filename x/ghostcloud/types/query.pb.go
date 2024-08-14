c2823e7be6376e777063eb604e0902f3c1fc3bcb
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-08-14 11:15:52 -0400

    chore(deps): bumps

--- x/ghostcloud/types/query.pb.go
+++ x/ghostcloud/types/query.pb.go
@@ -508,6 +508,7 @@ func _Query_Content_Handler(srv interface{}, ctx context.Context, dec func(inter
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "ghostcloud.ghostcloud.Query",
 	HandlerType: (*QueryServer)(nil),
